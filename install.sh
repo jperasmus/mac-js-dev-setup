dc7f90c2e4407099e369149d04c8fee6b809d3b8
JP Erasmus <jperasmus11@gmail.com>
2017-02-19 15:59:19 +0200

    Setup `install.sh` script to install nvm and kick off other tasks

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
-# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
-echo 'test';
+
+# download nvm installation script
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | NVM_DIR=/usr/local/nvm bash && \
+# sources your bashrc to add nvm to path
+source ~/.bashrc && \
+# check the nvm use message
+command -v nvm && \
+# install most recent nodejs stable version
+nvm install node && \
+# list installed node version
+# nvm ls && \
+# use stable as current version
+nvm use node && \
+# list all the node versions you can install
+# nvm ls-remote && \
+# set the installed stable version as the default node
+nvm alias default node && \
+
+# if we reach this point we have everything we need to kick the setup off
+node index.js
